8e88cfe665559bf64ebd5ef9dce80b16088a58fb
Tomas Szwarcberg <tszwarcberg@fi.uba.ar>
2022-02-26 15:34:08 -0300

    Theme cleanup

--- src/assets/themes/theme.js
+++ src/assets/themes/theme.js
@@ -7,23 +7,17 @@ const config = {
 };
 
 const colors = {
-	brand: "green.400",
-};
-
-const fontSizes = {
-	xs: "0.75rem",
-	sm: "0.875rem",
-	md: "1rem",
-	lg: "1.125rem",
-	xl: "1.25rem",
-	"2xl": "1.5rem",
-	"3xl": "1.875rem",
-	"4xl": "2.25rem",
-	"5xl": "3rem",
-	"6xl": "3.75rem",
-	"7xl": "4.5rem",
-	"8xl": "6rem",
-	"9xl": "8rem",
+	primary: "green.400",
+	styles: {
+		global: props => ({
+			"html, body, #root": {
+				height: "100%",
+			},
+			"*::-webkit-scrollbar": {
+				display: "none",
+			},
+		}),
+	},
 };
 
 const breakpoints = createBreakpoints({
@@ -56,41 +50,17 @@ const spacing = {
 	14: "4.5rem",
 	15: "5rem",
 	16: "6rem",
-};
-
-const sizes = {
-	max: "max-content",
-	min: "min-content",
-	full: "100%",
-	"3xs": "14rem",
-	"2xs": "16rem",
-	xs: "20rem",
-	sm: "24rem",
-	md: "28rem",
-	lg: "32rem",
-	xl: "36rem",
-	"2xl": "42rem",
-	"3xl": "48rem",
-	"4xl": "56rem",
-	"5xl": "64rem",
-	"6xl": "72rem",
-	"7xl": "80rem",
-	"8xl": "90rem",
-	container: {
-		sm: "640px",
-		md: "768px",
-		lg: "1024px",
-		xl: "1280px",
-	},
+	17: "7rem",
+	18: "9rem",
+	19: "10rem",
+	20: "11rem",
 };
 
 const theme = extendTheme({
 	config,
 	colors,
-	fontSizes,
 	breakpoints,
 	spacing,
-	sizes,
 });
 
 export default theme;

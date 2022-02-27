949307c6ab89f44f83933fbceb4679be8285b1a4
Tomas Szwarcberg <tszwarcberg@fi.uba.ar>
2022-02-27 01:32:36 -0300

    Documents Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,20 @@ import NavBar from "./NavBar";
 import { Flex } from "@chakra-ui/react";
 import SideBar from "./SideBar";
 
+/*
+Layout component puts page content under the navbar and at the right side of sidebar
+To do this, wrap the content inside <Layout></Layout>
+E.g:
+
+const DashboardContainer = () => {
+    return (
+        <Layout>
+            <Dashboard />
+        </Layout>
+    );
+}
+*/
+
 const Layout = ({ children }) => {
 	return (
 		<>
